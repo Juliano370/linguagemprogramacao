e744ae4c9be57383a8280b2df32d8fe37edd066e
Juliano370 <julianomendes28@yahoo.com>
2025-02-18 08:18:29 -0300

    meu primeiro programa web

---
+++ lingprog20251-main/js/scripts.js
@@ -0,0 +1,13 @@
+function seguir(){
+    alert("voce agora esta seguindo juliano")
+}
+
+/*
+1 git init
+2. git add
+3. git config user email "your-email"
+4.git config user. name  "your-name"
+5.gitcommit-m "meu primeiro programa web"
+6. git remote add  origin https//gitub.com/df piresling
+
+*/
